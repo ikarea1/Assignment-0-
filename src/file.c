876396b04fe848911874b3b5be7dd7fb4a3b1f5e
Oskar <oskarug@gmail.com>
2020-09-06 11:36:19 +0200

    XML

--- src/file.c
+++ src/file.c
@@ -4,25 +4,28 @@
 #include <string.h>
 #include <errno.h>
 
-// Med den her kommando kan koden afprøves: "make file && ./file testFile.c && echo $?"
-// Ved at følge arbejdssedlen kan i også prøve at køre det uden noget filnavn: "make file && ./file && echo $?"
 enum file_type {
     DATA,
     EMPTY,
     ASCII,
 };
 
+/// <summary> The function print_error takes the path of a file and serves as a print error </summary>
+/// <return> Returns a print statement containing a path and errnum </return>
 int print_error(char *path, int errnum) {
     return fprintf(stdout, "%s: cannot determine (%s)\n",
     path, strerror(errnum));
 }
 
+/// <summary> Takes the first argument and if there only is one argc it will exit with failure </summary>
+/// <return> Returns a EXIT_FAILURE and prints and error message </return> 
 int main (int argc, char* argv[]) {
     char* filename = argv[1];
     if (argc == 1) {
         printf("Usage: %s\n", filename);
         return(EXIT_FAILURE);
     }
+
     FILE* f_in = fopen(filename, "r");
 
     if (f_in == NULL) {
@@ -31,11 +34,17 @@ int main (int argc, char* argv[]) {
     }
     enum file_type var;
 
+    /// <summary> Checks if the first character is the end of the file, and in that case set enum variable to EMPTY </summary>
+    /// <returns> Returns the enum variable var </returns> 
     char c = fgetc(f_in);
     if (c == EOF) {
         var = 1;
     }
-    // Prøv at bruge char *fgets(char *str, int n, FILE *stream), den vil måske løse problemet.
+
+    /// <summary> It checks if it is end of file and if it not, it will access either flow chart and determines if it is 
+    /// either ASCII text of data and sets the enum variable var to either DATA or ASCII </summary>
+    /// <return> It updates the variable and if it accesses the else loop it will terminate the while
+    /// loop and return data </return>
     while (c != EOF) {
         if ((c >= 0x07 && c <= 0x0D) || (c == 0x1B) || (c >= 0x20 && c <= 0x7E)) { 
             c = fgetc(f_in);
@@ -47,6 +56,8 @@ int main (int argc, char* argv[]) {
         }
     }
 
+    /// <summary> A switch statement on the enum variable var that decides which case and a filename plus data type </summary>
+    /// <returns> Prints a filename and data type </returns>
     switch (var)
     {
     case DATA: printf("%s: data\n", filename); break;
